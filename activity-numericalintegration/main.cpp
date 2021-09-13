8e89df17bc5be9037a3fc592cbe20ba4d867b23e
willisreid96 <77948816+willisreid96@users.noreply.github.com>
2021-09-12 23:09:05 -0400

    Update main.cpp

--- activity-numericalintegration/main.cpp
+++ activity-numericalintegration/main.cpp
@@ -16,13 +16,45 @@ float f4(float x, int intensity);
 }
 #endif
 
-  
-int main (int argc, char* argv[]) {
+  int main (int argc, char* argv[]) {
 
-  if (argc < 6) {
+    if (argc < 6) {
     std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity>"<<std::endl;
     return -1;
   }
+
+  float x = 0.0;
+  int id = atoi(argv[1]);
+  float a = atof(argv[2]);
+  float b = atof(argv[3]);
+  int n = atoi(argv[4]);
+  int i = atoi(argv[5]);
+  float intergral = 0.0;
+  float func = (b-a)/n;
+  for(int c=0; c<n-1; c++) {
+    switch (id) {
+    case 1:
+      intergral += f1(a+(c+.5)*func, c) * func;
+      break;
+    case 2:
+      intergral += f2(a+(c+.5)*func, c) * func;
+      break;
+    case 3:
+      intergral += f3(a+(c+.5)*func, c) * func;
+      break;
+    case 4:
+      intergral += f4(a+(c+.5)*func, c) * func;
+      break;
+    default:
+      break;
+    }
+  }
+  
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<double> t = end-start;
+  std::cout << integral << "\n";
+  std::cerr << t.count() <<" seconds\n";
+
   
   return 0;
 }
